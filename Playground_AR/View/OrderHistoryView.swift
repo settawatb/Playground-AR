5fad66089698977d1bf09b2a737eaf544abfefb9
Settawat B <90975874+settawatb@users.noreply.github.com>
2024-04-24 22:29:02 +0700

    improve ux-ui in addproductview

---
+++ Playground_AR/View/OrderHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderHistoryView.swift
+//  Playground_AR
+//
+//  Created by Kisses MJ on 24/4/2567 BE.
+//
+
+import SwiftUI
+
+struct OrderHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderHistoryView()
+}
